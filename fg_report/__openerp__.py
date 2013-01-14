2abe1f09ffa6cad4fd1ee4d8b390a443cdaa32b5
zero <wangsong1233276@126.com>
2013-01-14 21:26:32 +0800

    X

---
+++ fg_report/__openerp__.py
@@ -0,0 +1,20 @@
+
+# -*- encoding: utf-8 -*-
+
+{
+    'name': '仪表盘',
+    'version': '1.0',
+    'category' : '富光',
+    'description': """富光仪表盘系统""",
+    'author': 'Daniel',
+    'website': 'http://www.fuguang.cn',
+    'depends': ['base', 'fg_sale', 'fg_account'],
+    'init_xml': [],
+    'update_xml': [
+        
+    ],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+    'application':True,
+}
